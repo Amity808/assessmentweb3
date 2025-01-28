93d0b9779d751fa8596db7a5282c6852a8b51382
Amity808 <bolarinwamuhdsodiq0@gmail.com>
2025-01-28 20:52:45 +0100

    update code

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,9 @@ function App() {
   const [depositAmount, setDepositAmount] = useState(0);
 
   const [amountWithdraw, setAmountWithdraw] = useState(0);
-  const [isloading, setIsloading] = useState(false)
-  const [balance, setBalance] = useState(0)
-  const [connectState, setConnectState] = useState(false)
+  const [isloading, setIsloading] = useState(false);
+  const [balance, setBalance] = useState(0);
+  const [connectState, setConnectState] = useState(false);
 
   const contractAddress = "0xe238b320A22af4c4EAf607cf30843e6eB7de567C";
 
@@ -24,43 +24,59 @@ function App() {
   }
 
   const handleDeposit = async (e) => {
+    setIsloading(true);
+
     if (typeof window.ethereum !== "undefined") {
       await requestAccount();
       const provider = new ethers.BrowserProvider(window.ethereum);
-      if(provider) {
-        setConnectState(true)
+      if (provider) {
+        setConnectState(true);
       }
       const signer = await provider.getSigner();
-      const contract = new ethers.Contract(contractAddress,assessmentAbi, signer);
-
+      const contract = new ethers.Contract(
+        contractAddress,
+        assessmentAbi,
+        signer
+      );
       try {
         const tx = await contract.deposit(depositAmount);
         const reciept = await tx.wait();
         toast.success("Deposited Successfully");
+        setIsloading(false);
+        // return reciept;
       } catch (error) {
         toast.error("Deposited Error, Unxepected error");
+        setIsloading(false);
       }
     }
-
   };
 
   const handleWithdraw = async () => {
+    setIsloading(true);
     if (typeof window.ethereum !== "undefined") {
       await requestAccount();
       const provider = new ethers.BrowserProvider(window.ethereum);
-      
-      if(provider) {
-        setConnectState(true)
+
+      if (provider) {
+        setConnectState(true);
       }
       const signer = await provider.getSigner();
-      const contract = new ethers.Contract(contractAddress,assessmentAbi, signer);
+      const contract = new ethers.Contract(
+        contractAddress,
+        assessmentAbi,
+        signer
+      );
 
       try {
         const tx = await contract.withdraw(amountWithdraw);
         const reciept = await tx.wait();
         toast.success("Withdraw Successfully");
+
+        setIsloading(false);
+        // return reciept;
       } catch (error) {
         toast.error("Withdraw Error, Unxepected error");
+        setIsloading(false);
       }
     }
   };
@@ -69,58 +85,86 @@ function App() {
     if (typeof window.ethereum !== "undefined") {
       await requestAccount();
       const provider = new ethers.BrowserProvider(window.ethereum);
-      if(provider) {
-        setConnectState(true)
+      if (provider) {
+        setConnectState(true);
       }
       const signer = await provider.getSigner();
-      const contract = new ethers.Contract(contractAddress,assessmentAbi, signer);
+      const contract = new ethers.Contract(
+        contractAddress,
+        assessmentAbi,
+        signer
+      );
 
       try {
         const tx = await contract.getBalance();
-        const formatedBalance = await ethers.formatEther(tx)
+        const formatedBalance = await ethers.formatEther(tx);
         setBalance(formatedBalance);
-        toast.success("Deposited Successfully");
-        // return reciept
+        toast.success("Balance fetched");
       } catch (error) {
-        toast.error("Deposited Error, Unxepected error");
+        toast.error("Unxepected error");
       }
     }
   };
 
   return (
     <>
-    <div className="flex justify-center flex-col m-10">
+      <div className="flex justify-center flex-col m-10">
+        <div className="">
+          <div className=" mb-5">
+            {/* {connectState ? <p>Wallet Connected</p> : <p>Connect Wallet</p>} */}
+          </div>
+          <div className=" mb-5">
+            <input
+              type="text"
+              value={depositAmount}
+              onChange={(e) => setDepositAmount(e.target.value)}
+              placeholder="input deposit amount"
+              className=" p-2 border-2 rounded-2xl mr-5"
+            />
+            {!isloading ? (
+              <button
+                className=" bg-black py-2 px-2 text-white rounded-md"
+                disabled={isloading}
+                onClick={handleDeposit}
+              >
+                Deposit
+              </button>
+            ) : (
+              <button
+                className=" bg-black py-2 px-2 text-white rounded-md"
+              >
+                Depositing
+              </button>
+            )}
+          </div>
 
-      <div className="">
-        <div className=" mb-5">
-    {connectState ? <p>Wallet Connected</p> : <p>Connect Wallet</p>}
+          <div className=" mb-5">
+            <input
+              type="text"
+              value={amountWithdraw}
+              onChange={(e) => setAmountWithdraw(e.target.value)}
+              placeholder="withdraw amount"
+              className=" p-2 border-2 rounded-2xl mr-5"
+            />
+            <button
+              className=" bg-black py-2 px-2 text-white rounded-md"
+              disabled={isloading}
+              onClick={handleWithdraw}
+            >
+              Withdraw
+            </button>
+          </div>
         </div>
-        <div className=" mb-5">
-          <input
-            type="text"
-            value={depositAmount}
-            onChange={(e) => setDepositAmount(e.target.value)}
-            placeholder="input deposit amount" className=" p-2 border-2 rounded-2xl mr-5"
-          />
-          <button className=" bg-black py-2 px-2 text-white rounded-md" onClick={handleDeposit}>Deposit</button>
-        </div>
-
-        <div className=" mb-5">
-          <input
-            type="text"
-            value={amountWithdraw}
-            onChange={(e) => setAmountWithdraw(e.target.value)}
-            placeholder="withdraw amount" className=" p-2 border-2 rounded-2xl mr-5"
-          />
-          <button className=" bg-black py-2 px-2 text-white rounded-md" onClick={handleWithdraw}>Withdraw</button>
+        <div>
+          <h3>Current Balance: {balance}</h3>
+          <button
+            className=" bg-black py-2 px-2 text-white rounded-md"
+            onClick={getBalance}
+          >
+            Get Balance
+          </button>
         </div>
       </div>
-      <div>
-        <h3>Current Balance: {balance}</h3>
-        <button className=" bg-black py-2 px-2 text-white rounded-md" onClick={getBalance}>Get Balance</button>
-      </div>
-      </div>
-
     </>
   );
 }
